d8a94e9a90870a651426a941cdb0ff794981723c
ty_pwd <ty_pwd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-12 18:17:21 +0000

    adding missing files
    git-svn-id: https://plugins.svn.wordpress.org/weekly-class-schedule/trunk@450354 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wcs_update.php
@@ -0,0 +1,43 @@
+<?php
+
+class WcsUpdate {
+		
+	public $table_name;
+	
+	public function __construct() {
+		global $wpdb;
+		
+		$this->table_name = $wpdb->prefix . "wcs_options";
+		$sql = "CREATE TABLE " . $this->table_name . " (
+  	 			id int(11) NOT NULL AUTO_INCREMENT,
+  	 			option_name VARCHAR(155) NOT NULL,
+  	 			option_value VARCHAR(155) NOT NULL,
+  	 			UNIQUE KEY id (id)
+  				);";
+		
+		dbDelta( $sql );
+	}
+	
+	public function update_version_number_in_database( $version ) {
+		global $wpdb;
+		$sql = "SELECT option_name FROM " . $this->table_name . " WHERE option_name = 'version'";
+		$options = $wpdb->get_results( $sql );
+		if ( $options ) {
+			$wpdb->update( 
+				$this->table_name, 
+				array( 'option_name' => 'version', 'option_value' => $version ), 
+				array( 'option_name' => 'version' ),
+				array( '%s', '%s' )
+			);
+		} else {
+			$wpdb->insert( 
+				$this->table_name, 
+				array( 'option_name' => 'version', 'option_value' => $version ), 
+				array( '%s', '%s' )
+			);
+		}
+	}
+	
+	
+	
+}
