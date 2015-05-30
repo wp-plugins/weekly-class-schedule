3e6b1d093b75180e8e3c37fdb3bff3cf04845940
ty_pwd <ty_pwd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-30 07:01:36 +0000

    added ex1 link to settings
    git-svn-id: https://plugins.svn.wordpress.org/weekly-class-schedule/trunk@1170782 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/wcs3_settings.php
+++ includes/wcs3_settings.php
@@ -110,6 +110,19 @@ function wcs3_standard_settings_page_callback() {
                 <td><?php wcs3_bool_checkbox( 'wcs3_allow_html_in_notes', $wcs3_options['allow_html_in_notes'], __('Yes') ); ?></td>
             </tr>
         </table>
+
+        <?php if ( ! is_plugin_active( 'wcs-expansion-pack-1/wcs-ex1.php') ) : ?>
+            <div class="ex1-link-box">
+                <p>Check out the new <a href="http://pulsarwebdesign.com/downloads/weekly-class-schedule-expansion-pack-1" target="_blank">Expansion Pack</a> for additional features such as graphical representation of class duration, per class/instructor/location color customization, responsive layout, and more.</p>
+
+                <p>
+                    <img src="<?php echo WCS3_PLUGIN_URL; ?>/img/expansion-pack-1-graphic-624x315.jpg" alt="Expansion pack 1 graphic" />
+                </p>
+                <p style="text-align: center">
+                    <a class="button button-primary" href="http://pulsarwebdesign.com/downloads/weekly-class-schedule-expansion-pack-1" target="_blank">Check it out!</a>
+                </p>
+            </div>
+        <?php endif; ?>
         
         <h3> <?php _e( 'Appearance Settings', 'wcs3' ); ?></h3>
         <table class="form-table">
@@ -177,7 +190,7 @@ function wcs3_standard_settings_page_callback() {
                 <td><?php wcs3_colorpicker( 'wcs3_color_links', $wcs3_options['color_links'] ) ?></td>
             </tr>
         </table>
-        
+
         <?php submit_button( __( 'Save Settings' ) ); ?>
         <?php wp_nonce_field( 'wcs3_save_options', 'wcs3_options_nonce' ); ?>
     </form>
